186de559eb2f738c9c7d72eff058e24cd79844c4
Liss63 <s.lipatov@mail.ru>
2021-03-19 13:19:30 +0400

    добавил указание интерпретатора

--- moonshinerservice.py
+++ moonshinerservice.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
 import multiprocessing
 
 import time
 import random
 from threading import Timer
 import platform
+from moonshinermenu import MoonshineMenuItem
 
 if platform.machine() == 'armv6l':
     import tempsensors
@@ -15,7 +17,9 @@ else:
 
 import RPi.GPIO as GPIO
 
-#button board mode pin 11,13,15
+
+# button board mode pin 11,13,15
+
 
 class RepeatTimer(Timer):
     def run(self):
@@ -41,27 +45,36 @@ def temp_sensors_process(sensors_dict):
             sensors_dict['1'] = 23.4 + random.random()
             sensors_dict['2'] = 27.5 + random.random()
         time.sleep(0.1)
-    return
 
 
 def button_callback(channel):
     print('This is a edge event callback function!')
-    print('Edge detected on channel %s'%channel)
+    print('Edge detected on channel %s' % channel)
     print('This is run in a different thread to your main program')
 
 
 def button_config():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # green button
+    GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # red button
+    GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # white button
     GPIO.add_event_detect(11, GPIO.RISING, callback=button_callback, bouncetime=200)
     GPIO.add_event_detect(13, GPIO.RISING, callback=button_callback, bouncetime=200)
     GPIO.add_event_detect(15, GPIO.RISING, callback=button_callback, bouncetime=200)
 
+
+def menu_config():
+    m = MoonshineMenuItem("Меню")
+    m.add_submenu(MoonshineMenuItem("Датчики"))
+    m.add_submenu(MoonshineMenuItem("Настройки"))
+    return m
+
+
 if __name__ == '__main__':
     print("start")
     button_config()
+    menu = menu_config()
+    print(menu)
     manager = multiprocessing.Manager()
     d = manager.dict()
     p = multiprocessing.Process(target=temp_sensors_process, args=(d, ))
@@ -74,5 +87,3 @@ if __name__ == '__main__':
             p.terminate()
             break
     p.join()
-
-
